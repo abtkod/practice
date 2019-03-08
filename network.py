cd47f15307474efe4e8f3e86756d7c312fef40ed
Abtin@OSU <abtinkh+github@gmail.com>
2019-03-08 15:25:02 -0800

    regularization

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 """
 **Reminder**: The general methodology to build a Neural Network is to:
@@ -16,16 +17,34 @@ class DeepNetwork(object):
     # activation functions with derivatives
     SIGMOID = lambda z, derivative=False: 1/(1 + np.exp(-z)) if not derivative else np.exp(-z)/(1+np.exp(-z))**2
     TANH = lambda z, derivative=False: np.tanh(z) if not derivative else 1 - np.tanh(z)**2
-    RELU = lambda z, derivative=False: np.where(z<=0, 0, z) if not derivative else np.where(z<=0, 0, 1)    
+    RELU = lambda z, derivative=False: np.where(z<=0, 0, z) if not derivative else np.where(z<=0, 0, 1)
     
     # it may even works with multiple unit output layer
     loss = lambda yhat, y, derivative=False: (-1) * (y * np.log(yhat) + (1-y) * np.log(1-yhat)) if not derivative else\
                                             (-1)*y/yhat + (1-y)/(1-yhat)
-    cost = lambda yhat, y: 1/y.shape[1] * np.sum(DeepNetwork.loss(yhat, y), axis=1)
+            
+    cost = lambda yhat, y, regularization_value: 1/y.shape[1] * np.sum(DeepNetwork.loss(yhat, y), axis=1) + regularization_value
+    
+    # regularization
+    class Regularization:
+        def __init__(self, kind):
+            self.kind = kind
+                
+        def weight_decay(self, **kwargs):
+            assert set(['alpha', 'lambd', 'm']).issubset(set(kwargs.keys())), 'invalid parameters'             
+            if self.kind == 'L2':
+                return (1 - kwargs['alpha'] * kwargs['lambd'] / kwargs['m'])
+                
+        def cost_adjustment(self, **kw):
+            assert set(['lambd', 'm', 'W']).issubset(kw.keys()), 'invalid parameters'            
+            if kw['lambd'] == 0.0:
+                return 0.0
+            if self.kind == 'L2':
+                return kw['lambd']/kw['m']/2 * reduce((lambda x,y: x+y), map(lambda W: np.linalg.norm(W, ord='fro')**2, kw['W']))        
 
-
-    def __init__(self, units_per_layer, activation_functions):
+    def __init__(self, units_per_layer, activation_functions, regularization_type='L2'):
         assert len(units_per_layer) == len(activation_functions), 'layer mismatch'
+        self._regular = self.Regularization(regularization_type)
         self.units_per_layer = list(units_per_layer)
         self.activation_functions = list(activation_functions)
         self._W = None
@@ -39,7 +58,7 @@ class DeepNetwork(object):
     def b(self):
         return self._b[1:]
         
-    def train(self, X, Y, alpha, max_iterations, terminate_on_cost_change=0.00000001, print_cost_every=100)->np.array:
+    def train(self, X, Y, alpha, max_iterations, lambd=0.0, terminate_on_cost_change=0.00000001, print_cost_every=100)->np.array:
         assert X.shape[1] == Y.shape[1], 'invalid input'
         
         np.random.seed(1)
@@ -74,10 +93,12 @@ class DeepNetwork(object):
                 dW[l] = 1/m * np.dot(dZ[l], A[l-1].T)
                 db[l] = 1/m * np.sum(dZ[l], axis=1, keepdims=True)
 
-                W[l] = W[l] - alpha * dW[l]
+                wdec = self._regular.weight_decay(**{'alpha':alpha, 'lambd':lambd, 'm':m})
+                W[l] = wdec * W[l] - alpha * dW[l] # (1 - alpha*lambd/m) is the weight decay coming from L2 regularization
                 b[l] = b[l] - alpha * db[l]
             
-            new_cost = DeepNetwork.cost(A[L], Y)    
+            reg_val = self._regular.cost_adjustment(**{'lambd':lambd, 'm':m, 'W':W[1:]})
+            new_cost = DeepNetwork.cost(A[L], Y, regularization_value=reg_val)
             if 0 < cost - new_cost < terminate_on_cost_change:
                 cost = new_cost
                 break
