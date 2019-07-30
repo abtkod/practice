7515cfcae8f8c01332a25d8c04122b0f44944657
Abtin@OSU <abtinkh+github@gmail.com>
2019-07-29 17:17:23 -0700

    binary search tree

--- interview/datastructure/tree.py
+++ interview/datastructure/tree.py
@@ -1,8 +1,28 @@
 from datastructure.node import *
 
-class BinaryTreeNode(Node):
+class BinaryTree(Node):
     def __init__(self, val):                
-        Node.__init__(self, val, max_child_count=2)
+        super().__init__(val, max_child_count=2)
+        
+    def replace_child(self, cindex, child):
+        #overriding Node's replace_child method to ensure every nodes has at most one parent.
+        assert 0 <= cindex < len(self._children), 'Invalid child index'
+        assert isinstance(child, type(self)), f'New child must be a subclasses of {type(self)}'
+        if self._children[cindex] is not None:
+            # remove current child
+            self.remove_child(cindex)
+        self._children[cindex] = child
+        # this is the main difference between BinaryTreeNode and Node.
+        child._parents = [self]
+        
+    def access(self, idxlst:list):
+        nd = self
+        try:            
+            for i in idxlst:
+                nd = nd[i]
+        except TypeError:        
+            nd = -1
+        return nd
                 
     def __repr__(self):
         rep = {}        
@@ -10,14 +30,16 @@ class BinaryTreeNode(Node):
             if level not in rep.keys():
                 rep[level] = list()
             if node is None:                
-                rep[level].append('null')
+                rep[level].append(None)
                 return
             
             rep[level].append(Node.__repr__(node))            
             for c in node._children:                
                 subtree_repr(c, rep, level+1)
         subtree_repr(self, rep, 0)
-        return 'tree: ' + repr(rep)
+        if rep[max(rep)].count(None) == len(rep[max(rep)]):
+            rep.pop(max(rep))
+        return f'{self.__class__.__name__}<depth={max(rep)}>: ' + repr(rep)
     
     def __str__(self):        
         def subtree_str(node):                                
@@ -29,4 +51,85 @@ class BinaryTreeNode(Node):
                 val_str += subtree_str(c) + ', '
             val_str = val_str[:-2] + ')'
             return val_str
-        return subtree_str(self)
+        return f'{self.__class__.__name__}: ' + subtree_str(self)
+
+
+class BinarySearchTree:
+    def __init__(self, dtype):
+        self._dtype = dtype
+        self._root = None
+        
+    def insert(self, val):
+        if self._root is None:            
+            self._root = BinaryTree(val)
+            return self
+        assert isinstance(val, self._root._dtype), 'Invalid data type'
+        p = self._root
+        while True:
+            if val <= p.value and p[0] is None:
+                p[0] = BinaryTree(val)
+                break
+            if val > p.value and p[1] is None:
+                p[1] = BinaryTree(val)
+                break            
+            p = p[0] if val <= p.value else p[1]
+        return self
+    
+    def search(self, val):
+        if self._root.value == val:
+            return ['root']
+        ndidx = []
+        p = self._root
+        while p is not None:
+            if p.value == val:
+                return ndidx
+            if val < p.value:
+                ndidx.append(0)
+                p = p[0]
+            else:
+                ndidx.append(1)
+                p = p[1]        
+        return -1
+    
+    def access(self, idxlst:list):
+        if idxlst == ['root']:
+            return self._root
+        return self._root.access(idxlst)
+    
+    def remove(self, val):
+        idx = self.search(val)
+        if idx == -1:
+            return -1
+        nd = self.access(idx)
+        
+        if nd != self._root:
+            par_nd = nd._parents[0]
+        else:
+            # create a dummy Node and then remove it at the end of the function
+            par_nd = BinaryTree(val)
+            par_nd[0] = nd
+        ndidx = 0 if par_nd[0] == nd else 1
+                
+        if nd.child_count() == 0:
+            par_nd.remove_child(ndidx)
+            if nd == self._root:                
+                self._root = None
+            return self
+        
+        subidx = 1 if nd[1] is not None else 0
+        lookupidx = 0 if nd[1] is not None else 1
+        par_nd.replace_child(ndidx, nd[subidx])    
+        if nd[lookupidx] is not None:
+            sub = nd[subidx]
+            while sub[lookupidx] is not None:
+                sub = sub[lookupidx]
+            sub[lookupidx] = nd[lookupidx]
+        if nd == self._root:
+            self._root = nd[subidx]
+        return self
+    
+    def __repr__(self):
+        return repr(self._root)
+    
+    def __str__(self):
+        return str(self._root)
