43077b09ea75b2661c41252f680add0d8c8063a2
Abt@Win <abtinkh+github@gmail.com>
2020-03-20 16:43:38 +0330

    1709KthMultiple improved

--- interview/CtCI/Chapter 17/1709KthMultiple.py
+++ interview/CtCI/Chapter 17/1709KthMultiple.py
@@ -2,11 +2,11 @@ import unittest
 from collections import deque
 
 def kth_multiple(k):
-	three, five, seven = deque([1,3]), deque([5]), deque([7])
-	output = []
-	d = set()
-	i = 0
-	while i < k:
+	three, five, seven = deque([3]), deque([5]), deque([7])
+	output = [1]
+	d = set([1])
+	i = 1
+	while i < k:		
 		if three[0] <= five[0] and three[0] <= seven[0]:
 			val = three.popleft()
 		elif five[0] <= three[0] and five[0] <= seven[0]:
@@ -15,8 +15,8 @@ def kth_multiple(k):
 			val = seven.popleft()
 		if val not in d:
 			three.append(3 * val)
-			five.append(5 * val)
-			seven.append(7 * val)
+			five.append(5 * val) if val%3 != 0 else None
+			seven.append(7 * val) if val%3 !=0 and val%5 != 0 else None
 			output.append(val)
 			d.add(val)
 			i += 1
