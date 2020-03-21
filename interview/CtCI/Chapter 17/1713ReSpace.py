db6830e6cc82e94419eb2f26eea9d240d64234fb
Abt@Win <abtinkh+github@gmail.com>
2020-03-21 18:59:29 +0430

    1713ReSpace.py

--- interview/CtCI/Chapter 17/1713ReSpace.py
+++ interview/CtCI/Chapter 17/1713ReSpace.py
@@ -1,28 +1,31 @@
 import unittest
+import string
 
 class Trie:
-	pass
+	def __init__(self):
+		pass
 	
 
 def re_space(document, dictionary:Trie):
-	def unconcatenate(doc, dic, memo):
+	max_word_size = dictionary.max_length()
+	def unconcatenate(start, doc, dic, memo):
 		if doc is None:
 			return 0, ''
 		if doc in memo:
-			return memo[doc]
+			return memo[start]
 		candidates = []
 		# assuming that first character is not unrecongnized
-		unkcount, spaced = unconcatenate(doc[1:], dic, memo)
+		unkcount, spaced = unconcatenate(start + 1, doc, dic, memo)
 		unkcount += 1
-		candidates.append((unkcount, doc[0] + ' ' + spaced))
+		candidates.append((unkcount, doc[start] + ' ' + spaced))
 		# assuming that there is a word that starts with first character
-		words = dic.getwords(doc)
+		words = dic.getwords(doc[start: start + max_word_size])
 		for w in words:
-			unkcount, spaced = unconcatenate(doc[len(w):], dic, memo)
+			unkcount, spaced = unconcatenate(start + len(w), doc, dic, memo)
 			candidates.append((unkcount, w + ' ' + spaced))
-		memo[doc] = min(candidates)
-		return memo[doc]
+		memo[start] = min(candidates)
+		return memo[start]
 		
-	return unconcatenate(document, dictionary, {})
+	return unconcatenate(0, document, dictionary, [None]*len(document))
 	
 			
