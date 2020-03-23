ea1fba6acad7ca9228ac0558e1c57937358e727d
Abt@Win <abtinkh+github@gmail.com>
2020-03-23 10:18:33 +0430

    optimal iterative version

--- interview/CtCI/Chapter 17/1716Masseuse.py
+++ interview/CtCI/Chapter 17/1716Masseuse.py
@@ -1,6 +1,6 @@
 import unittest
 
-def the_masseuse(appointments):
+def the_masseuse(appointments): # O(n) time, O(n) space
 	def optimal(apps, start, memo={}):
 		if start in memo:
 			return memo[start]
@@ -19,6 +19,16 @@ def the_masseuse(appointments):
 		return memo[start]
 	return optimal(appointments, 0)
 
+def the_masseuse_iterative(appointments): # O(n) time, O(1) space
+	one_away = two_away = 0
+	for i in range(len(appointments)-1, -1, -1):
+		bestwith = appointments[i] + two_away
+		bestwithout = one_away
+		current = max(bestwith, bestwithout)
+		two_away = one_away
+		one_away = current
+	return one_away
+
 
 class Test(unittest.TestCase):
 	data = [30, 15, 60, 75, 45, 15, 15, 45]
@@ -26,7 +36,8 @@ class Test(unittest.TestCase):
 		seq = the_masseuse(self.data)
 		print(seq)
 		self.assertEqual(sum(seq), 180)
-
+	def test_the_masseuse_iterative(self):
+		self.assertEqual(the_masseuse_iterative(self.data), 180)
 
 if __name__ == '__main__':
 	unittest.main()
