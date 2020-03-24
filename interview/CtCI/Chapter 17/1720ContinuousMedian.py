58434fb49411b3f295996b76be1afbb5ab1d50b1
Abt@Win <abtinkh+github@gmail.com>
2020-03-24 13:07:18 +0430

    working

--- interview/CtCI/Chapter 17/1720ContinuousMedian.py
+++ interview/CtCI/Chapter 17/1720ContinuousMedian.py
@@ -1,28 +1,44 @@
 import unittest
+import heapq
 
 
 class Heap:
 	def __init__(self, type='min'):
-		pass
+		assert (type == 'min' or type == 'max'), 'invalid type of heap'
+		self._op = 1 if type == 'min' else -1
+		self.head = []
+	
+	def __str__(self):
+		return str([self._op * v for v in self.head])
+
+	def __len__(self):
+		return len(self.head)
+
+	@property
+	def top(self):
+		if len(self.head) != 0:
+			return self._op * self.head[0]
+		else:
+			return None
+
+	def insert(self, val):
+		assert(len(self.head) == 0 or type(val) == type(self.head[0])), 'invalid type of object'
+		heapq.heappush(self.head, self._op * val)
+
+	def extract_top(self):
+		return self._op * heapq.heappop(self.head)	
 
 
-def continuous_median(random_generator):
-	if len(random_generator) == 1 or len(random_generator) == 2:
-		numbers = [num for num in random_generator]
+def continuous_median(numbers):	
+	assert (len(numbers) > 0), 'empty list'
+	if len(numbers) < 3:
 		return numbers, numbers[0]
 
 	minh = Heap('min')
 	maxh = Heap('max')
-	numbers = []
-	for i, num in enumerate(random_generator):
-		numbers.append(num)
-		if i == 0:
-			minh.insert(num)
-			continue
-		if i == 1:
-			maxh.insert(num)
-			continue
-
+	minh.insert(max(numbers[:2]))
+	maxh.insert(min(numbers[:2]))
+	for num in numbers[2:]:
 		if num >= minh.top:
 			minh.insert(num)
 		else:
@@ -33,16 +49,33 @@ def continuous_median(random_generator):
 		elif len(minh) - len(maxh) < -1 :
 			top = maxh.extract_top()
 			minh.insert(top)
-	median = maxh.top if len(maxh) >= len(minh) else minh.top
-	return numbers, median
+	if len(maxh) == len(minh):
+		median = (minh.top + maxh.top)/2
+	elif len(maxh) > len(minh):
+		median = maxh.top
+	else:
+		median = minh.top
+	return median
 
 
-class Test(unittest.TestCase):
-	def test_continuous_median(self):
-		import random
-		gen = (random.randint(1, 99) for _ in range(7))
-		numbers, median = continuous_median(gen)
-		mm = sorted(numbers)[len(numbers) // 2]
+import random
+class Test(unittest.TestCase):	
+
+	def test_heap(self):
+		l = [random.randint(-10, 10) for _ in range(20)]
+		minh, maxh = Heap('min'), Heap('max')
+		for v in l:
+			minh.insert(v)
+			maxh.insert(v)		
+		self.assertEqual(min(l), minh.top)
+		self.assertEqual(max(l), maxh.top)
+			
+
+	def test_continuous_median(self):		
+		numbers = [random.randint(1, 99) for _ in range(7)]
+		median = continuous_median(numbers)
+		numbers.sort()
+		mm = numbers[len(numbers) // 2] if len(numbers)%2 == 1 else (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2])/2
 		self.assertEqual(median, mm)
 
 
