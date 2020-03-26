9aa6e5665abbcd686f75057e05173894fa7b8aa7
Abt@Win <abtinkh+github@gmail.com>
2020-03-26 23:33:08 +0430

    optimized version

--- interview/CtCI/Chapter 17/1722WordTransformer.py
+++ interview/CtCI/Chapter 17/1722WordTransformer.py
@@ -16,7 +16,69 @@ def word_transformer(word1, word2, dictionary):
 						path += [w]
 						w = visited[w]
 					return path[::-1]
-		wordstovisit = newwords			
+		wordstovisit = newwords
+
+from collections import deque
+def word_transformer_optimal(word1, word2, dictionary):
+	def get_groups(word):
+		groups = []
+		for i in range(len(word)):
+			groups.append(word[:i] + '_' + word[i+1:])
+		return groups
+
+	def get_adjacent_groups(group, grouptowords):
+		adjgroups = set()
+		for word in grouptowords[group]:
+			adjgroups.update(get_groups(word))
+		adjgroups.remove(group)
+		return adjgroups
+
+	def transformation(path, group):
+		def fill_gaps(w1, w2):
+			ix = w1.find('_')
+			return w1.replace('_', w2[ix])			
+
+		while path[group][0]:
+			gp = group
+			group = path[group][0]
+			path[group][1] = gp		
+		transform = []
+		while path[group][1]:
+			word = fill_gaps(group, path[group][1])
+			transform += [word]
+			group = path[group][1]
+		return transform
+
+	gptoword = {}
+	gen = (word for word in dictionary if len(word) == len(word1))
+	for word in gen:
+		for gp in get_groups(word):
+			gptoword.setdefault(gp, [])
+			gptoword[gp].append(word)
+	tovisit1, tovisit2 = deque(get_groups(word1)), deque(get_groups(word2))
+	path = {**{gp: [word1, None] for gp in tovisit1}, 
+			**{gp: [None, word2] for gp in tovisit2}}
+	path[word1], path[word2] = [None, None], [None, None]
+	while len(tovisit1) != 0 or len(tovisit2) != 0:
+		if len(tovisit1) != 0:			
+			group = tovisit1.popleft()
+			for gp in get_adjacent_groups(group, gptoword):
+				path.setdefault(gp, [None, None])
+				if path[gp][0] is None:
+					path[gp][0] = group
+					tovisit1.append(gp)
+				if path[gp][1] is not None:
+					return transformation(path, gp)
+		if len(tovisit2) != 0:			
+			group = tovisit2.popleft()
+			for gp in get_adjacent_groups(group, gptoword):				
+				path.setdefault(gp, [None, None])
+				if path[gp][1] is None:
+					path[gp][1] = group
+					tovisit2.append(gp)
+				if path[gp][0] is not None:
+					return transformation(path, gp)	
+	return False
 
 
 class Test(unittest.TestCase):
@@ -26,6 +88,9 @@ class Test(unittest.TestCase):
 			path = word_transformer('DAMP', 'LIKE', self.dictionary)
 			self.assertEqual(path, ['DAMP', 'LAMP', 'LIMP', 'LIME', 'LIKE'])
 
+		def test_word_transformer_optimal(self):
+			path = word_transformer_optimal('DAMP', 'LIKE', self.dictionary)
+			self.assertEqual(path, ['DAMP', 'LAMP', 'LIMP', 'LIME', 'LIKE'])
 
 if __name__ == '__main__':
 	unittest.main()
